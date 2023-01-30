ef95ae6e7d4931f3f47524381c6d68a4318522a4
jeff-vincent <jeff.d.vincent@gmail.com>
2023-01-30 14:11:16 -0800

    add config script for argo job template

--- src/job/config_and_run.sh
+++ src/job/config_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i '' 's/aws-access-key-id/'$AWS_ACCESS_KEY_ID'/g' s3-workflow.yaml
-sed -i '' 's/aws-secret-access-key/'$AWS_SECRET_ACCESS_KEY/'g' s3-workflow.yaml
-sed -i '' 's/s3-raw-images/'$S3_RAW_IMAGES'/g' s3-workflow.yaml
-sed -i '' 's/s3-processed-images/'$S3_PROCESSED_IMAGES'/g' s3-workflow.yaml
+sed -i 's/aws-access-key-id/'$AWS_ACCESS_KEY_ID'/g' s3-workflow.yaml
+sed -i 's|aws-secret-access-key|'$AWS_SECRET_ACCESS_KEY'|g' s3-workflow.yaml
+sed -i 's|s3-raw-images|'$S3_RAW_IMAGES'|g' s3-workflow.yaml
+sed -i 's/s3-processed-images/'$S3_PROCESSED_IMAGES'/g' s3-workflow.yaml
 
 python main.py
